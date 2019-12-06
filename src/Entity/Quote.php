cc78566a1bbf9b9ef1ab8c7a825823bcc170a8d3
MOHAMED CHAKIRI <yassir.chakiri12@gmail.com>
2019-12-06 16:11:34 +0100

    optimization entities getters & setters

--- src/Entity/Quote.php
+++ src/Entity/Quote.php
@@ -2,10 +2,10 @@
 
 class Quote
 {
-    public $id;
-    public $siteId;
-    public $destinationId;
-    public $dateQuoted;
+    private $id;
+    private $siteId;
+    private $destinationId;
+    private $dateQuoted;
 
     public function __construct($id, $siteId, $destinationId, $dateQuoted)
     {
@@ -15,13 +15,77 @@ class Quote
         $this->dateQuoted = $dateQuoted;
     }
 
-    public static function renderHtml(Quote $quote)
+    public function renderHtml(Quote $quote)
     {
-        return '<p>' . $quote->id . '</p>';
+        return '<p>' . $quote->getId() . '</p>';
     }
 
-    public static function renderText(Quote $quote)
+    public function renderText(Quote $quote)
     {
-        return (string) $quote->id;
+        return (string) $quote->getId();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSiteId()
+    {
+        return $this->siteId;
+    }
+
+    /**
+     * @param mixed $siteId
+     */
+    public function setSiteId($siteId)
+    {
+        $this->siteId = $siteId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDestinationId()
+    {
+        return $this->destinationId;
+    }
+
+    /**
+     * @param mixed $destinationId
+     */
+    public function setDestinationId($destinationId)
+    {
+        $this->destinationId = $destinationId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateQuoted()
+    {
+        return $this->dateQuoted;
+    }
+
+    /**
+     * @param mixed $dateQuoted
+     */
+    public function setDateQuoted($dateQuoted)
+    {
+        $this->dateQuoted = $dateQuoted;
     }
 }
